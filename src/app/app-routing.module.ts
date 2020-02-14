fe94163afb6a28c4919aef5e256e1f225812a608
Pierre Augustin <Pierre@News-MBP.home>
2020-02-13 20:19:29 -0500

    APPCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 import { AppComponent as FormulaPage} from './components/formulaPage/app/app.component';
 import { TempleteAppComponent as TempletePage} from './components/formulaPage/templete-app/templete-app.component';
 import { AppComponent as ResourcePage} from './components/resourcePage/app/app.component';
-import { AppComponent as ProjectPage} from './components/resourcePage/app/app.component';
+import { AppComponent as ProjectPage} from './components/projectPage/app/app.component';
 import { LoginPageComponent} from './components/homepage/login-page/login-page.component';
-import { NavbarComponent} from './components/homepage/navbar/navbar.component';
+
 import { AppComponent as Home} from './components/homepage/app/app.component';
 
 
@@ -14,7 +14,7 @@ import { CommonModule } from '@angular/common';
 const routes:Routes = [
  
   {path: "", component: Home}, // CONTAINS  THE HEADER/NAVBAR/FOOTER
-  {path: "homePage", component: LoginPageComponent}, // CONTAINS THE LOGIN PAGE (HOME PAGE)(APP)
+  {path: "", component: LoginPageComponent}, // CONTAINS THE LOGIN PAGE (HOME PAGE)(APP)
   {path: "resourcesPage", component: ResourcePage}, // CONTAINS THE RESOURCES PAGE(APP)
   {path: "projectPage", component: ProjectPage}, // CONTAINS THE PROJECT PAGE(APP)
   {path: "formulaPage", component: FormulaPage}, // CONTAINS THE FORMALA PAGE(APP)
--- src/app/components/homepage/app/app.component.ts
+++ src/app/components/homepage/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import {HeaderComponent} from '../header/header.component';
+// import {FooterComponent} from '../footer/footer.component';
+import {NavbarComponent} from '../navbar/navbar.component';
+
 @Component({
   selector: 'app-app',
   templateUrl: './app.component.html',
