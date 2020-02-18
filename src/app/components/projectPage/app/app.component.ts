a04e806810ba88f56b1ff02704ece2402d8d393a
ianantonius17 <ianantonius17@gmail.com>
2020-02-18 09:41:41 -0500

    project page tuesday

--- src/app/components/projectPage/app/app.component.ts
+++ src/app/components/projectPage/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {range} from 'rxjs';
+import {newArray} from '@angular/compiler/src/util';
+import {dataTable} from '../../../../assets/dataTable.js';
+import {AppService} from './app.service';
 
 @Component({
   selector: 'app-app',
@@ -7,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() { }
 
-  dummy = ['1', '2', '3']
+  // Variables
+  projectData;
+  selected;
+  deleteSelected;
+  show;
+  checked: boolean;
+  checkAll: boolean;
+  uncheckAll: boolean;
+
+  page;
+  itemPerPage = 20;
+
+
+  // Functions
+  changeMade;
+  changeMadeToDelete;
+  transferToShow;
+  deleteFromShow;
+  selectAll;
+  unselectAll;
+  submitProject;
+
+
+  constructor(appService: AppService) {
+    this.projectData = appService.projectData;
+    this.show = appService.show;
+    this.selected = appService.selected;
+    this.deleteSelected = appService.deleteSelected;
+    this.checkAll = appService.checkAll;
+    this.uncheckAll = appService.uncheckAll;
+    this.changeMade = appService.changeMade;
+    this.changeMadeToDelete = appService.changeMadeToDelete;
+    this.transferToShow = appService.transferToShow;
+    this.deleteFromShow = appService.deleteFromShow;
+    this.selectAll = appService.selectAll;
+    this.unselectAll = appService.unselectAll;
+  }
   ngOnInit(): void {
   }
+
 
 }
